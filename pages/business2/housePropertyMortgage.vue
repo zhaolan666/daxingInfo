31c1f72653250145f7b46d54cd85214d2ee28e27
mingdu <wy1412185335@163.com>
2023-03-26 13:48:05 +0800

    completed housePropertyMortgage data insert and layout construction

--- pages/business2/housePropertyMortgage.vue
+++ pages/business2/housePropertyMortgage.vue
@@ -17,7 +17,7 @@
 			<sb-line></sb-line>
 			<view class=" flex padv6 padh20">
 				<template v-if="!showMore">
-					<view class="flex mar30 marv10" v-for="d in dictTop5">
+					<view class="flex mar30 marv10" v-for="(d,i) in dictTop5" :key="i">
 						<view class="font27">
 							{{d.type}}
 						</view>
@@ -27,7 +27,7 @@
 				</template>
 
 				<template v-else>
-					<view class="flex mar30 marv10" v-for="d in dict">
+					<view class="flex mar30 marv10" v-for="(d,i) in dict" :key="i">
 						<view class="font27">
 							{{d.type}}
 						</view>
@@ -52,7 +52,7 @@
 			<view class="flex pav6 padh20">
 				<scroll-view scroll-x>
 					<view class="flex flex-nowrap">
-						<u-tag :text="`条件${i+1}`" v-for="(item, i) in 14" size="mini" type="error" closable>
+						<u-tag :text="`条件${i+1}`" v-for="(item, i) in 14" :key="i" size="mini" type="error" closable>
 						</u-tag>
 					</view>
 				</scroll-view>
@@ -60,54 +60,36 @@
 		</view>
 		<!-- p3 -->
 		<view class="flex j-s bg-white">
-			<view style="width: 200rpx;">
+			<view style="width: 240rpx;">
 				<view class="th j-a-c flex c-w" style="background-color: #0Ec7ab;">
 					产品
 				</view>
-				<view class="product flex j-a-c" v-for="(item, i) in 10">
-					<image style="width: 60rpx; height: 60rpx;"
-						src="http://43.142.29.179:7700/public/xlxxzx_imgs/nyyh.png"></image>
-					<view class="padh6"></view>
-					中国
+				<view class="product flex j-a-c" v-for="(item, i) in products" :key="i">
+					<template v-if="item.icon">
+						<image style="width: 60rpx; height: 60rpx;"
+							:src="`http://43.142.29.179:7700/public/xlxxzx_imgs/banks/${item.icon}`">
+						</image>
+						<view class="padh6"></view>
+					</template>
+					<view class="font26">
+						{{item.name}}
+					</view>
 				</view>
 			</view>
 
-			<scroll-view scroll-x style="width: calc(100vw - 202rpx);">
+			<scroll-view scroll-x style="width: calc(100vw - 242rpx);">
+				<!-- 表头 -->
 				<view class="flex flex-nowrap">
-					<view class="">
-						<view class="th j-a-c flex c-w">
-							额度（万）
-						</view>
-						<view class="product flex j-a-c" v-for="(item, i) in 10" style="color:orange">
-							200-300
-						</view>
+					<view class="th j-a-c flex c-w" v-for="(c,i) in columns" :key="i">
+						{{c.label}}
 					</view>
-					<view class="">
-						<view class="th j-a-c flex c-w">
-							利率（%）
-						</view>
-						<view class="product flex j-a-c" v-for="(item, i) in 10" style="color:#3c8e0b">
-							3.25
-						</view>
-					</view>
-					<view class="">
-						<view class="th j-a-c flex c-w">
-							还款方式
-						</view>
-						<view class="product flex j-a-c padh10" v-for="(item, i) in 10" style="color:#0e4fd4">
-							1年期先息后本3年期先息后本年还本金10%
-						</view>
-					</view>
-					<view class="">
-						<view class="th j-a-c flex c-w">
-							额度（万）
-						</view>
-						<view class="product flex j-a-c" v-for="(item, i) in 10" style="color:orange">
-							200-300
-						</view>
-					</view>
+				</view>
 
-
+				<!-- 内容 -->
+				<view class="flex flex-nowrap" v-for="(b,i) in products" :key="i">
+					<view class="product flex j-a-c" v-for="(c, j) in columns" :key="j" style="color:orange">
+						{{b[c.key]}}
+					</view>
 				</view>
 			</scroll-view>
 		</view>
@@ -204,21 +186,118 @@
 		"type": "抵押准入",
 		"values": ["1押", "2押"]
 	}]
+
+	let columns = [{
+		"key": "额度(万)",
+		"label": "额度(万)"
+	}, {
+		"key": "利率%",
+		"label": "利率%"
+	}, {
+		"key": "还款方式",
+		"label": "还款方式"
+	}, {
+		"key": "抵押准入",
+		"label": "抵押准入"
+	}, {
+		"key": "最高成数",
+		"label": "最高成数"
+	}, {
+		"key": "借款人年龄(岁)",
+		"label": "借款人年龄(岁)"
+	}, {
+		"key": "借款人身份",
+		"label": "借款人身份"
+	}, {
+		"key": "查询次数",
+		"label": "查询次数"
+	}, {
+		"key": "逾期",
+		"label": "逾期"
+	}, {
+		"key": "权利人",
+		"label": "权利人"
+	}, {
+		"key": "房产类型",
+		"label": "房产类型"
+	}, {
+		"key": "房龄(年)",
+		"label": "房龄(年)"
+	}, {
+		"key": "面积(m2)",
+		"label": "面积(m2)"
+	}, {
+		"key": "小孩房",
+		"label": "小孩房"
+	}, {
+		"key": "持证时间",
+		"label": "持证时间"
+	}, {
+		"key": "备用房",
+		"label": "备用房"
+	}, {
+		"key": "营业执照(月)",
+		"label": "营业执照(月)"
+	}, {
+		"key": "持股时间(月)",
+		"label": "持股时间(月)"
+	}, {
+		"key": "流水覆盖(倍)",
+		"label": "流水覆盖(倍)"
+	}, {
+		"key": "进入行业(天)",
+		"label": "进入行业(天)"
+	}, {
+		"key": "放款方式",
+		"label": "放款方式"
+	}, {
+		"key": "放款账户",
+		"label": "放款账户"
+	}, {
+		"key": "流程",
+		"label": "流程"
+	}, {
+		"key": "资料",
+		"label": "资料"
+	}]
+
 	export default {
+		onShareAppMessage() {
+
+		},
+		onShareTimeline() {
+
+		},
 		data() {
 			return {
 				dict,
+				products: [],
+				columns,
 				showMore: false
 			}
 		},
 		methods: {
-
+			getList() {
+				uni.request({
+					url: 'http://43.142.29.179:7700/public/xlxxzx_imgs/products.json',
+					success: ({
+						statusCode,
+						data
+					}) => {
+						if (statusCode === 200) {
+							this.products = data
+						}
+					}
+				})
+			}
 		},
 		computed: {
-
 			dictTop5() {
 				return dict.slice(0, 5)
 			}
+		},
+		onReady() {
+			this.getList()
 		}
 	}
 </script>
@@ -226,7 +305,7 @@
 <style lang="scss" scoped>
 	.th {
 		background-color: #17BeDc;
-		min-width: 200rpx;
+		min-width: 240rpx;
 		border: 1px solid #ddd;
 		border-bottom: 1px solid #ddd;
 		height: 70rpx;
@@ -234,7 +313,7 @@
 
 	.product {
 		height: 250rpx;
-		min-width: 200rpx;
+		min-width: 240rpx;
 		border-right: 1px solid #ddd;
 		border-bottom: 1px solid #ddd;
 	}
